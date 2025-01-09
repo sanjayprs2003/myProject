da33f74259a0a42115177adf32e01abe7fd2f6df
sanjayprs2003 <sanjayprs2003@gmail.com>
2025-01-09 17:21:10 +0530

    Docker PRoject

--- src/main/java/expense_tracker/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package expense_tracker.repository;
-
-import expense_tracker.model.PhotoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<PhotoModel, Integer> {
-}
